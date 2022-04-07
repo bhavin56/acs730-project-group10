b35c35bf1ec3a3ca97cd55729759c13392f4886d
bhavin <bhavin@gmail.com>
2022-04-07 05:06:52 +0000

    Limiting open ingres,egress to specific IPs

--- terraform/modules/SG/main.tf
+++ terraform/modules/SG/main.tf
@@ -10,17 +10,19 @@ resource "aws_security_group" "sg_lb" {
   vpc_id      = var.vpc_id
 
   ingress {
+    description = "port open for loadbalancer"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp" # HTTP access from anywhere
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.59.184/32"]
   }
 
   egress {
+    description = "Outbound allowed"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Outbound Rules
+    cidr_blocks = ["100.26.225.49/32"] # Outbound Rules
   }
 
   tags = merge(local.default_tags, {
@@ -46,15 +48,15 @@ resource "aws_security_group" "bastion_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["172.31.59.184/32"]  
   }
 
   egress {
+    description      = "Outbound allowed"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["100.26.225.49/32"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -71,17 +73,19 @@ resource "aws_security_group" "webserver_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
+    description = "port open for loadbalancer"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp" # HTTP access from anywhere
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.59.184/32"]
   }
 
   egress {
+    description      = "Outbound allowed"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["100.26.225.49/32"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
