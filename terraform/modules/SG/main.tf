1d23f53d87b09b35742c1f4da76f038e3329de38
raman5552 <raman@raman>
2022-04-14 15:18:55 +0000

    Adding bastion and key pair

--- terraform/modules/SG/main.tf
+++ terraform/modules/SG/main.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["172.31.59.184/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -61,7 +61,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["100.26.225.49/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
